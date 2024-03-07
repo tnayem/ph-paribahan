866b9507b6f0303fdddc2d30dac29a93081556a4
tnayem <tasdidnayem@gmail.com>
2024-03-08 01:29:59 +0600

    final push

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const seatInfo = document.getElementById('seat-info');
 const couponBtn = document.getElementById('apply-copun');
 let totalPrice = document.getElementById('totalPrice');
 let grandTotalPrice = document.getElementById('grandTotalPrice')
+let totalDiscountAmount = document.getElementById('totalDiscount')
 let bookingSeats = []
 const allSeats = document.getElementsByClassName('seat')
 for (const seat of allSeats) {
     seat.addEventListener("click", function (e) {
-        seat.style.background = 'rgb(29 209 0 / var(--tw-bg-opacity))'
+        
         if (e.target.innerText === bookingSeats[0] || e.target.innerText === bookingSeats[1] || e.target.innerText === bookingSeats[2] || e.target.innerText === bookingSeats[3]) {
             alert("This seat already selected")
         } else {
@@ -18,6 +19,7 @@ for (const seat of allSeats) {
             if (quantityBukingSeats > 4) {
                 alert("You can Selected only 4 seats")
             } else {
+                seat.style.background = 'rgb(29 209 0 / var(--tw-bg-opacity))'
                 // remaining seats calculation start
                 const totalRemainingSeats = remainingSeats - quantityBukingSeats;
                 document.getElementById('remaining-seats').innerText = totalRemainingSeats;
@@ -45,21 +47,27 @@ for (const seat of allSeats) {
                 grandTotalPrice.innerText = totalSitPrice;
                 // coupon code calculation
                 if(quantityBukingSeats=== 4){
-                    document.getElementById('apply-copun').disabled = false
+                    const applyCopon = document.getElementById('apply-copun')
+                    applyCopon.disabled = false
                     couponBtn.addEventListener('click',function(){
                       let  couponCode = document.getElementById('coupon')
                       let couponCodeValue = couponCode.value;
                       if(couponCodeValue === "NEW15"){
                         const discount = 0.15;
                         const totalDiscount = (quantityBukingSeats * 550) * discount;
+                        totalDiscountAmount.innerText = totalDiscount;
                         grandTotalPrice.innerText = totalSitPrice - totalDiscount;
+                        applyCopon.disabled = true
                         couponCode.value = " "
                       }
-                      if(couponCodeValue === "Couple 20"){
+                      else if(couponCodeValue === "Couple 20"){
                         const coupleDiscount = 0.2
                         const totalcoupleDiscount = (quantityBukingSeats * 550) * coupleDiscount;
                         grandTotalPrice.innerText = totalSitPrice - totalcoupleDiscount;
+                        applyCopon.disabled = true
                         couponCode.value = " "
+                      }else{
+                        alert("This Coupon Code is Not Valid")
                       }
                       
                     })
@@ -74,9 +82,11 @@ const numberValue = number.value
 const name = document.getElementById('name');
 const email = document.getElementById('email')
 const nextButton = document.getElementById('nextButton')
-console.log(numberValue);
+// if(bookingSeats.length > 0){
+//     number.disabled = false;
+// }
 number.addEventListener('input',function(e){
-    if(e.target.value.length>7){
+    if(e.target.value.length>7 && bookingSeats.length > 0){
         nextButton.disabled = false
     }
 })
